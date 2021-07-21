c12bbc1fc1d8be478d61d71b1a66ca58c88b74fa
zzre <rgc00@korea.ac.kr>
2021-07-21 19:43:33 +0900

    32bit .exe file PEviewer

--- PEviewer.c
+++ PEviewer.c
@@ -151,6 +151,16 @@ char *IMAGE_SECTION_HEADER_CHARACTERISTICS[6] = {
     "IMAGE_SCN_MEM_WRITE"                   // 0x80000000
 };
 
+char *IMAGE_IMPORT_DESCRIPTOR_NAMES[5] = {
+    "OriginalFirstThunk",
+    "TimeDateStamp",
+    "ForwarderChain",
+    "Name",
+    "FirstThunk"
+};
+
+int IMAGE_IMPORT_DESCRIPTOR_SIZES[5] = { 4, 4, 4, 4, 4 };
+
 void printNbytes(char*, int);
 
 int main(int argc, char *argv[])
@@ -163,6 +173,14 @@ int main(int argc, char *argv[])
     int NT_HEADER_OFFSET = 0;
     int NUMBER_OF_SECTIONS = 0;
     int NUMBER_OF_RVA_AND_SIZES = 0;
+    int IMAGE_SECTION_HEADER_OFFSET;
+    int IMPORT_DIRECTORY_RVA;
+    int VirtualAddress;
+    int PointerToRawData;
+    int IMAGE_IMPORT_DESCRIPTOR_OFFSET;
+    int NAME_RVA, NAME_OFFSET;
+    int IMAGE_IMPORT_BY_NAME_OFFSET; // INT
+    int IMPORT_ADDRESS_TABLE_OFFSET; // IAT 
     char ch1[4], ch2[4];
 
     if (argc != 2)
@@ -184,7 +202,6 @@ int main(int argc, char *argv[])
 
     memcpy(data, buf, 2);
     data[2] = 0;
-    NT_HEADER_OFFSET = *(int*)data;
     // printf("┌ 0x%08X\t%-10s\t%02X%02X\t%s\t/* DOS Signature */\n", offset, DOS_HEADER_NAMES[0], *data, *(data+1), data);
     printf("┌ 0x%08X\t%-10s\t%04X\t%s\t/* DOS Signature */\n", offset, DOS_HEADER_NAMES[0], *(int*)data, data);
     offset += 2;
@@ -258,7 +275,7 @@ int main(int argc, char *argv[])
     for (int i=0; i<29; i++)
     {
         memcpy(data, buf+(offset-NT_HEADER_OFFSET), IMAGE_OPTIONAL_HEADER_SIZES[i]);
-        //data[IMAGE_OPTIONAL_HEADER_SIZES[i]] = 0;
+        data[IMAGE_OPTIONAL_HEADER_SIZES[i]] = 0;
         printf("   ├ 0x%08X\t%-25s\t", offset, IMAGE_OPTIONAL_HEADER_NAMES[i]);
         printNbytes(data, IMAGE_OPTIONAL_HEADER_SIZES[i]);
         printf("\n");
@@ -305,6 +322,7 @@ int main(int argc, char *argv[])
 
     /* IMAGE_SECTION_HEADER */
     printf("[+] IMAGE_SECTION_HEADER\n");
+    IMAGE_SECTION_HEADER_OFFSET = offset;
     prev_offset = offset;
     fseek(fp, NT_HEADER_OFFSET+6, SEEK_SET);
     fread(&NUMBER_OF_SECTIONS, sizeof(char), 2, fp);
@@ -312,71 +330,167 @@ int main(int argc, char *argv[])
 
 
     for (int n=1; n<=NUMBER_OF_SECTIONS; n++)
-{
-    fseek(fp, prev_offset, SEEK_SET);
-    fread(buf, sizeof(char), 0x38, fp);
+    {
+        fseek(fp, prev_offset, SEEK_SET);
+        fread(buf, sizeof(char), 0x38, fp);
 
-    memcpy(data, buf+(offset-prev_offset), IMAGE_SECTION_HEADER_SIZES[0]);
-    if (n == 1 && n == NUMBER_OF_SECTIONS)
-    {
-        strcpy(ch1, "─");
-        strcpy(ch2, " ");
-    }
-    else if (n == 1)
-    {
-        strcpy(ch1, "┌");
-        strcpy(ch2, "│");
-    }
-    else if (n == NUMBER_OF_SECTIONS)
-    {
-        strcpy(ch1, "└");
-        strcpy(ch2, " ");
-    }
-    else
-    {
-        strcpy(ch1, "├");
-        strcpy(ch2, "│");
-    }
-
-    printf("%s─ IMAGE_SECTION_HEADER %s\n", ch1, data);
-    printf("%s  ├ 0x%08X\t%-30s\t", ch2, offset, IMAGE_SECTION_HEADER_NAMES[0]);
-    printNbytes(data, IMAGE_SECTION_HEADER_SIZES[0]);
-    printf("\t%s\n", data);
-    offset += IMAGE_SECTION_HEADER_SIZES[0];
-
-    for (int i=1; i<9; i++)
-    {
-        printf("%s  ├ 0x%08X\t%-30s\t", ch2, offset, IMAGE_SECTION_HEADER_NAMES[i]);
-        memcpy(data, buf+(offset-prev_offset), IMAGE_SECTION_HEADER_SIZES[i]);
-        printNbytes(data, IMAGE_SECTION_HEADER_SIZES[i]);
-        printf("\n");
-        offset += IMAGE_SECTION_HEADER_SIZES[i];
-    }
-    
-    printf("%s  └ 0x%08X\t%-30s\t", ch2, offset, IMAGE_SECTION_HEADER_NAMES[9]);
-    memcpy(data, buf+(offset-prev_offset), IMAGE_SECTION_HEADER_SIZES[9]);
-    printNbytes(data, IMAGE_SECTION_HEADER_SIZES[9]);
-    printf("\n");
-    offset += IMAGE_SECTION_HEADER_SIZES[9];
-
-    for (int i=0x20, count=0; i!=0x00000000; i<<=1, count++)
-    {
-        if (*(int*)data & i)
+        memcpy(data, buf+(offset-prev_offset), IMAGE_SECTION_HEADER_SIZES[0]);
+        if (n == 1 && n == NUMBER_OF_SECTIONS)
         {
-            if (i == 0x80000000 || ((*(int*)data/i) == 1))
-                printf("%s\t\t└ ", ch2);
-            else
-                printf("%s\t\t├ ", ch2);
-            printf("%s\n", IMAGE_SECTION_HEADER_CHARACTERISTICS[count]);
+            strcpy(ch1, "─");
+            strcpy(ch2, " ");
+        }
+        else if (n == 1)
+        {
+            strcpy(ch1, "┌");
+            strcpy(ch2, "│");
+        }
+        else if (n == NUMBER_OF_SECTIONS)
+        {
+            strcpy(ch1, "└");
+            strcpy(ch2, " ");
+        }
+        else
+        {
+            strcpy(ch1, "├");
+            strcpy(ch2, "│");
         }
 
-        if (i == 0x00000080)
-            i = 0x10000000;
-    }
-    prev_offset = offset;
-    printf("%s\n", ch2);
-}
+        printf("%s─ IMAGE_SECTION_HEADER %s\n", ch1, data);
+        printf("%s  ├ 0x%08X\t%-30s\t", ch2, offset, IMAGE_SECTION_HEADER_NAMES[0]);
+        printNbytes(data, IMAGE_SECTION_HEADER_SIZES[0]);
+        printf("\t%s\n", data);
+        offset += IMAGE_SECTION_HEADER_SIZES[0];
+
+        for (int i=1; i<9; i++)
+        {
+            printf("%s  ├ 0x%08X\t%-30s\t", ch2, offset, IMAGE_SECTION_HEADER_NAMES[i]);
+            memcpy(data, buf+(offset-prev_offset), IMAGE_SECTION_HEADER_SIZES[i]);
+            printNbytes(data, IMAGE_SECTION_HEADER_SIZES[i]);
+            printf("\n");
+            offset += IMAGE_SECTION_HEADER_SIZES[i];
+        }
     
+        printf("%s  └ 0x%08X\t%-30s\t", ch2, offset, IMAGE_SECTION_HEADER_NAMES[9]);
+        memcpy(data, buf+(offset-prev_offset), IMAGE_SECTION_HEADER_SIZES[9]);
+        printNbytes(data, IMAGE_SECTION_HEADER_SIZES[9]);
+        printf("\n");
+        offset += IMAGE_SECTION_HEADER_SIZES[9];
+
+        for (int i=0x20, count=0; i!=0x00000000; i<<=1, count++)
+        {
+            if (*(int*)data & i)
+            {
+                if (((*(int*)data/i) == 1))
+                    printf("%s\t\t└ ", ch2);
+                else
+                    printf("%s\t\t├ ", ch2);
+                printf("%s\n", IMAGE_SECTION_HEADER_CHARACTERISTICS[count]);
+            }
+
+            if (i == 0x00000080)
+                i = 0x10000000;
+        }
+        prev_offset = offset;
+        printf("%s\n", ch2);
+    }
+    
+    /* Import Address Table */
+    printf("[+] IMAGE_IMPORT_DESCRIPTOR\n");
+    offset = NT_HEADER_OFFSET + 0x80;
+    fseek(fp, offset, SEEK_SET);
+    fread(data, sizeof(char), 4, fp);
+    IMPORT_DIRECTORY_RVA = *(int*)data;
+
+    offset = IMAGE_SECTION_HEADER_OFFSET + 12;
+    fseek(fp, offset, SEEK_SET);
+    fread(data, sizeof(char), 12, fp);
+    VirtualAddress = *(int*)data;
+    PointerToRawData = *(int*)(data+8);
+
+    IMAGE_IMPORT_DESCRIPTOR_OFFSET = IMPORT_DIRECTORY_RVA - VirtualAddress + PointerToRawData;
+    offset = IMAGE_IMPORT_DESCRIPTOR_OFFSET;
+
+    /* IMAGE_IMPORT_DESCRIPTOR */
+    while (1) 
+    {
+        prev_offset = offset;
+        fseek(fp, offset, SEEK_SET);
+        fread(buf, sizeof(char), 24, fp);
+    
+        memcpy(data, buf+12, 4);
+        if ((*(int*)data) == 0)
+            break;
+
+        NAME_OFFSET = *(int*)data - VirtualAddress + PointerToRawData;
+        fseek(fp, NAME_OFFSET, SEEK_SET);
+        fread(data, sizeof(char), 20, fp);
+        printf("    %s\n", data);
+
+        memcpy(data, buf+(offset-prev_offset), IMAGE_IMPORT_DESCRIPTOR_SIZES[0]);
+        printf("    ┌ 0x%08X %-20s\t", offset, IMAGE_IMPORT_DESCRIPTOR_NAMES[0]);
+        printNbytes(data, 4);
+        printf("\n");
+        IMAGE_IMPORT_BY_NAME_OFFSET = *(int*)data - VirtualAddress + PointerToRawData;
+        offset += IMAGE_IMPORT_DESCRIPTOR_SIZES[0];
+
+        for (int i=1; i<4; i++)
+        {
+            memcpy(data, buf+(offset-prev_offset), IMAGE_IMPORT_DESCRIPTOR_SIZES[i]);
+            printf("    ├ 0x%08X %-20s\t", offset, IMAGE_IMPORT_DESCRIPTOR_NAMES[i]);
+            printNbytes(data, 4);
+            printf("\n");
+            offset += IMAGE_IMPORT_DESCRIPTOR_SIZES[i];
+        }
+        memcpy(data, buf+(offset-prev_offset), IMAGE_IMPORT_DESCRIPTOR_SIZES[4]);
+        printf("    ├ 0x%08X %-20s\t", offset, IMAGE_IMPORT_DESCRIPTOR_NAMES[4]);
+        printNbytes(data, 4);
+        printf("\n");
+        IMPORT_ADDRESS_TABLE_OFFSET = *(int*)data - VirtualAddress + PointerToRawData;
+        printf("    │\n");
+        printf("    └─────────── Import Address Table\n");
+
+        int off = IMAGE_IMPORT_BY_NAME_OFFSET;
+        int off2;
+        int ordinal;
+        int count = 0;
+        while (1)
+        {
+            fseek(fp, off, SEEK_SET);
+            fread(data, sizeof(char), 4, fp); // *(int*)data == Hint/Name RVA
+            if (*(int*)data == 0)
+                break;
+
+            printf("\t\t");
+            off2 = *(int*)data - VirtualAddress + PointerToRawData; // tmp = Hint/Name File offset
+
+            fread(data, sizeof(char), 4, fp);
+            if (*(int*)data == 0)
+            {
+                if (count == 0)
+                    printf("─ ");
+                else
+                    printf("└ ");
+            }
+            else if (count == 0)
+                printf("┌ ");
+            else
+                printf("├ ");
+
+            printf("0x%08X ", IMPORT_ADDRESS_TABLE_OFFSET+count*4);
+            fseek(fp, off2, SEEK_SET);
+            fread(&ordinal, sizeof(char), 2, fp);
+            fread(data, sizeof(char), 0x20, fp);
+            printf("%-30s\t(ordinal : %04X)\n", data, ordinal);
+
+            off += 4;
+            count++;
+        }
+
+        offset += IMAGE_IMPORT_DESCRIPTOR_SIZES[4];
+        printf("\n");
+    }
+
     fclose(fp);
 }
 
